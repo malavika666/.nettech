498674b77ef819c57e5bb76f2f47b232de24b08d
malavika666 <97940144+malavika666@users.noreply.github.com>
2022-03-03 11:31:09 +0530

    Add files via upload

---
+++ example1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DTOB
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the decimal number: ");
+            int number = int.Parse(Console.ReadLine());
+            int i;
+            int[] numberArray = new int[10];
+            for(i=0;number>0;i++)
+            {
+                numberArray[i] = number % 2;
+                number = number / 2;
+            }
+            Console.Write("binary representation of the given no: ");
+            for(i=i-1;i>=0;i--)
+            {
+                Console.Write(numberArray[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
