723beb3c63f3b88e08c066ceb41f7773e7e1323b
malavika666 <97940144+malavika666@users.noreply.github.com>
2022-03-03 11:37:06 +0530

    Add files via upload

---
+++ example2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the binary number: ");
+            int binaryNumber = int.Parse(Console.ReadLine());
+            int decimalValue = 0;
+            int base1 = 1;
+            while (binaryNumber > 0)
+            {
+                int reminder = binaryNumber % 10;
+                binaryNumber = binaryNumber / 10;
+                decimalValue += reminder * base1;
+                base1 = base1 * 2;
+            }
+            Console.Write($"decimal value:{decimalValue}");
+
+            Console.ReadKey();
+        }
+    }
+}
