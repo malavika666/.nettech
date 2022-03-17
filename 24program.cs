5ffd6a2a41ecd62e8babffcd45776e288c55e779
malavika666 <97940144+malavika666@users.noreply.github.com>
2022-03-17 11:19:55 +0530

    Add files via upload

---
+++ 24program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                panel1.Enabled = true;
+                App.PhoneBook.AddPhoneBookRow(App.PhoneBook.NewPhoneBookRow());
+                phoneBookBindingSource.MoveLast();
+                textBox3.Focus();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                App.PhoneBook.RejectChanges();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = true;
+            txtPhone.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            phonebookBindingSource.ResetBindings(false);
+            panel1.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 26program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputString, revstr = "";
+            int Length;
+            inputString = textBox1.Text;
+            Length = inputString.Length - 1;
+            while(Length>=0)
+            {
+                revstr = revstr + inputString[Length];
+                Length--;
+            }
+            MessageBox.Show("Reverse String Is:" + revstr, "Result");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string inputString;
+            inputString = textBox1.Text; 
+            MessageBox.Show("The String after Trimming:" + inputString.Trim(), "Result");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string inputString;
+            inputString = textBox1.Text;
+            inputString = inputString.PadLeft(10, '*');
+            inputString = inputString.PadRight(15, '*');
+            MessageBox.Show("String After Padding:" + inputString, "Result");
+
+        }
+    }
+}
